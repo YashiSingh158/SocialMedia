b036b7df954bd032aa1ed9322dba4100324a057d
Yashi Singh <yashisingh1563@gmail.com>
2024-12-08 19:16:39 +0530

    second-commit

--- frontend_socialMedia/src/environments/environment.prod.ts
+++ frontend_socialMedia/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+// For Production Environment
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://172.20.58.51:8080/api/v1',
+  defaultProfilePhotoUrl: 'src/assets/images/profile.jpg',
+	defaultCoverPhotoUrl: 'src/assets/images/cover.jpg'
 };
--- frontend_socialMedia/src/environments/environment.ts
+++ frontend_socialMedia/src/environments/environment.ts
@@ -1,6 +1,7 @@
+// For Development Environment
 export const environment = {
 	production: false,
 	apiUrl: 'http://localhost:8080/api/v1',
-	defaultProfilePhotoUrl: 'http://localhost:4200/assets/images/profile.jpg',
-	defaultCoverPhotoUrl: 'http://localhost:4200/assets/images/cover.jpg'
+	defaultProfilePhotoUrl: 'src/assets/images/profile.jpg',
+	defaultCoverPhotoUrl: 'src/assets/images/cover.jpg'
 };
